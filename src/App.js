35b7029ec26a940fea0abe61138eca30ae0c20bb
Thomas Emerson <emersonvisuals@gmail.com>
2024-04-07 14:09:29 +1000

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MaskedCursor from "./Components/MaskedCursor/MaskedCursor";
+
+function App() {
+    return (
+        <div className="App">
+            <MaskedCursor />
+        </div>
+    );
+}
+
+export default App;
